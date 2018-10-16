666135da05c02bf6ce93ff426e8707a08d430751
Alordiah Keno <Keno@Alordiahs-MacBook-Pro.local>
2018-10-16 21:31:34 +0100

    admin model like added

--- iconnect/admin.py
+++ iconnect/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from iconnect.models import Category, Conversation, Profile
+from iconnect.models import Category, Conversation, Profile, Like
 
 # Register your models here.
 class CategoryAdmin(admin.ModelAdmin):
@@ -8,3 +8,4 @@ class CategoryAdmin(admin.ModelAdmin):
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Conversation)
 admin.site.register(Profile)
+admin.site.register(Like)
